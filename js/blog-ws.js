7e4d407f7b71c0e7c12c161ce2184b436448eefb
momogaga <momodu06VL@hotmail.fr>
2015-01-24 12:51:36 +0100

    Affiche article with comments

--- js/blog-ws.js
+++ js/blog-ws.js
@@ -1,15 +1,3 @@
-
-$.ajax({
-    url: "http://localhost:8080/ProjectBlog/webresources/article",
-    type: "GET",
-    dataType: "json",
-    contentType: "application/json",
-    headers: {
-        Accept: "application/json"
-    },
-    success: [afficherArticle]
-});
-
 //ajout d'article
 $("#formArticle").submit(function (event) {
 
@@ -26,20 +14,37 @@ $("#formArticle").submit(function (event) {
     ajouterArticle(article);
 });
 
+$.ajax({
+    url: "http://localhost:8080/ProjectBlog/webresources/article",
+    type: "GET",
+    dataType: "json",
+    contentType: "application/json",
+    headers: {
+        Accept: "application/json"
+    },
+    success: [afficherArticle]
+});
 
 function afficherArticle(json) {
     var data = json;
     var contentHTML = '';
 
     $.each(data, function (i, item) {
-        contentHTML = "<h2 class='post-title'>"
-                + item.title + "</h2>" + "<h3 class='post-subtitle'>"
-                + item.content + "</h3>" + "<p class='post-meta'>Posté par " + item.a_ecrit.firstname + ' ' + item.a_ecrit.lastname + " le " + item.published_on + "</p>"
+        contentHTML = "<h2 class='post-title'>" + item.title + "</h2>"
+                + "<p class='post-subtitle'>" + item.content + "</p>";
+        for (var key in item.comments) {
+            if (item.comments[key] === undefined) {
+                contentHTML += "";
+            }
+            else {
+                contentHTML += "<blockquote>" + item.comments[key].comment + "</blockquote>"
+            }
+        }
+        contentHTML += "<p class='post-meta'>Posté par " + item.a_ecrit.firstname + ' ' + item.a_ecrit.lastname + " le " + item.published_on + "</p>"
                 + "<input id='textinput' name='textinput' type='text' placeholder='Ecrire un commentaire...' class='form-control input-md'><button class='btn btn-primary'>Commenter</button>"
                 + "<button class='btn btn-danger' onclick='supprimerArticle(" + item.id + ")'>Supprimer</button>"
                 + "<hr>";
 
-//console.log(contentHTML);
         $("#articlePoste").append(contentHTML);
     });
 }
